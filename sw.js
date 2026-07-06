7ff2866c97be4f6d47f6c6d99ae9e38d5387c1d0
khataatrans-debug <khat.aatrans@gmail.com>
2026-07-06 12:13:51 +0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,27 @@
+const CACHE = 'driver-v1';
+const ASSETS = [
+  '/driver.html',
+  '/app-config.js?v=4',
+  '/manifest.json',
+  '/icon-192.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
+  );
+});
